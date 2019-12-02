92d030aef933e3fd472e8658395516d2d2d7ac85
Alison Golden <alisong779@yahoo.com>
2019-12-02 17:45:01 -0500

    function for getTasks localStorage

--- app.js
+++ app.js
@@ -11,10 +11,31 @@ loadEventListeners();
 
 //Add a Task
 function loadEventListeners() {
+  document.addEventListener('DOMContentLoaded', getTasks);
   form.addEventListener('submit', addTask);
   taskList.addEventListener('click', removeTask);
   clearBtn.addEventListener('click', clearTasks);
   filter.addEventListener('keyup', filterTasks);
+}
+
+function getTasks() {
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function(task) {
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    li.appendChild(document.createTextNode(task));
+    const link = document.createElement('a');
+    link.className = 'delete-item secondary-content';
+    link.innerHTML = '<i class = "fa fa-remove"></i>';
+    li.appendChild(link);
+    taskList.appendChild(li);
+  });
 }
 
 function addTask(e) {
@@ -24,7 +45,6 @@ function addTask(e) {
   const li = document.createElement('li');
   li.className = 'collection-item';
   li.appendChild(document.createTextNode(taskInput.value));
-
   const link = document.createElement('a');
   link.className = 'delete-item secondary-content';
   link.innerHTML = '<i class = "fa fa-remove"></i>';
