957f5a97cd9b03bdabbe9a4f253e195b5b2f7d2c
Alison Golden <alisong779@yahoo.com>
2019-12-02 17:21:05 -0500

    event listeners and functions for remove and filter tasks

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
 //Load all event listeners
-
 loadEventListeners();
 
 //Add a Task
 function loadEventListeners() {
   form.addEventListener('submit', addTask);
+  taskList.addEventListener('click', removeTask);
+  clearBtn.addEventListener('click', clearTasks);
+  filter.addEventListener('keyup', filterTasks);
 }
 
 function addTask(e) {
@@ -26,9 +28,35 @@ function addTask(e) {
   const link = document.createElement('a');
   link.className = 'delete-item secondary-content';
   link.innerHTML = '<i class = "fa fa-remove"></i>';
-
   li.appendChild(link);
   taskList.appendChild(li);
+
   taskInput.value = '';
+
   e.preventDefault();
+}
+
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+function clearTasks() {
+  taskList.innerHTML = '';
+}
+
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
